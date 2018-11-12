d622de5407710f26ceb0ed6c14c9eaed5660797d
Daniel Kapper <dkapper01@gmail.com>
2018-11-12 13:28:33 -0500

    added api to Search.js and calling it form index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,8 @@
-import axios from "axios";
+import Search from './models/Search'; 
 
-import { times as t, add as a, ID } from "./views/SearchViews";
-import sting from "../js/models/Search";
+const search = new Search('pizza'); 
 
-async function getResuls(query) {
-  const key = "70615e47240e15c4ce77ff3535f534b1";
-  const res = await axios(
-    `https://www.food2fork.com/api/search?key=${key}&q=${query}`
-  );
-}
+search.getResuls('pizza'); 
+console.log(search); 
 
-console.log(`add ${a(4, 6)}, times ${t(5, 3)} ID = ${ID}`);
 
-// 70615e47240e15c4ce77ff3535f534b1
-// https://www.food2fork.com/api/search
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,21 @@
- 
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResuls() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "70615e47240e15c4ce77ff3535f534b1";
+    try {
+      const res = await axios(
+        `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
+
