d4f6bafd1dca7db92ec3784445a50dc70bc8d2d9
Daniel Kapper <dkapper01@gmail.com>
2018-11-12 13:54:14 -0500

    set up asyn controlSearch which calls getResuls();

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,28 @@
 import Search from './models/Search'; 
 
 const search = new Search('pizza'); 
-
-search.getResuls('pizza'); 
 console.log(search); 
+search.getResuls('pizza'); 
 
+const state = {
+
+}
+
+const controlSearch = async () => {
+  const query = 'pizza'; 
+
+  if(query) {
+    state.search = new Search(query); 
+
+    await state.search.getResuls(); 
+
+    console.log(state.search.result); 
+  } 
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault(); 
+  controlSearch(); 
+
+})
 
