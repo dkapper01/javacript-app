928bd678850757a43090c552464ae684b2a5c49c
Daniel Kapper <dkapper01@gmail.com>
2018-11-23 14:12:35 -0500

    Updated  recipe controller clearLoader() and renderRecipe view.

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Search from "./models/Search";
 import Recipe from "./models/Recipe";
 import { elements, renderLoader, clearLoader } from "./views/base";
 import * as SearchView from "./views/SearchViews";
+import * as recipeView from "./views/recipeView";
 
 const state = {};
 
@@ -11,7 +12,8 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Get query from view
-  const query = SearchView.getInput();
+  // const query = SearchView.getInput();
+  const query = "pizza";
 
   if (query) {
     // 2) New search object and add to state
@@ -22,16 +24,26 @@ const controlSearch = async () => {
     SearchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
 
-    // 5) Render results on UI
-    clearLoader();
-    SearchView.renderResults(state.search.result);
+      // 5) Render results on UI
+      clearLoader();
+      SearchView.renderResults(state.search.result);
+    } catch (err) {
+      alert("Something went wrong in Search Controller");
+      // clearLoader();
+    }
   }
 };
 
 elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
+// TEST
+elements.searchForm.addEventListener("load", e => {
   e.preventDefault();
   controlSearch();
 });
@@ -46,11 +58,46 @@ elements.searchResPages.addEventListener("click", e => {
 });
 
 /**
- *  Recipe Controller
- **/
+ * RECIPE CONTROLLER
+ */
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace("#", "");
 
-const r = new Recipe(23456);
+  if (id) {
+    // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
 
-r.getRecipe();
+    // Highlight selected search item
+    // if (state.search) searchView.highlightSelected(id);
 
-console.log(r); 
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+      
+    } catch (err) {
+      console.log(err);
+      alert("Error processing recipe!");
+    }
+  }
+};
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+
+// ["haschange", "load"].forEach(event =>
+//   window.addEventListener(event, controllerRecipe)
+// );
