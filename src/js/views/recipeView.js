6eaa1b823993a029b9063798034d0fa528bf1e79
Daniel Kapper <dkapper01@gmail.com>
2018-11-24 23:44:58 -0500

    add format count function

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,31 +1,32 @@
 import { elements } from "./base";
-// import { Fraction } from "fractional";
+import { Fraction } from "fractional";
 
 export const clearRecipe = () => {
   elements.recipe.innerHTML = "";
 };
-// const formatCount = count => {
-//   if (count) {
-//     // count = 2.5 --> 5/2 --> 2 1/2
-//     // count = 0.5 --> 1/2
-//     const newCount = Math.round(count * 10000) / 10000;
-//     const [int, dec] = newCount
-//       .toString()
-//       .split(".")
-//       .map(el => parseInt(el, 10));
 
-//     if (!dec) return newCount;
+const formatCount = count => {
+  if (count) {
+    // count = 2.5 --> 5/2 --> 2 1/2
+    // count = 0.5 --> 1/2
+    const newCount = Math.round(count * 10000) / 10000;
+    const [int, dec] = newCount
+      .toString()
+      .split(".")
+      .map(el => parseInt(el, 10));
 
-//     if (int === 0) {
-//       const fr = new Fraction(newCount);
-//       return `${fr.numerator}/${fr.denominator}`;
-//     } else {
-//       const fr = new Fraction(newCount - int);
-//       return `${int} ${fr.numerator}/${fr.denominator}`;
-//     }
-//   }
-//   return "?";
-// };
+    if (!dec) return newCount;
+
+    if (int === 0) {
+      const fr = new Fraction(newCount);
+      return `${fr.numerator}/${fr.denominator}`;
+    } else {
+      const fr = new Fraction(newCount - int);
+      return `${int} ${fr.numerator}/${fr.denominator}`;
+    }
+  }
+  return "?";
+};
 
 const createIngredient = ingredient => `
     <li class="recipe__item">
